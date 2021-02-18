7e8746904b89fd0ce6c89ec9e17f5a5ca1e328d8
claudio <clausspal97@gmail.com>
2021-02-17 21:26:57 -0400

    adding BasicMoves tests

--- ChessTests/BasicMoveTests.cs
+++ ChessTests/BasicMoveTests.cs
@@ -7,15 +7,33 @@ namespace ChessTests
 {
     public class BasicMoveTests : IDisposable
     {
+        private BasicMoveFakeClass subject;
         public BasicMoveTests()
         {
-            
+            subject = new BasicMoveFakeClass();
         }
         public void Dispose()
         {
-            throw new NotImplementedException();
+            subject = null;
         }
 
-        
+        [Fact]
+        void SetMove_WithKingAndSquareChessTable_ShouldChangeBasicMoveAttributes()
+        {
+            Chessman chessman = new King(new KingMovement());
+            ChessTable chessTable = new ChessTable(8, 8);
+            
+            
+        }
+    }
+
+    public class BasicMoveFakeClass : BasicMove
+    {
+        public override void SetChange()
+        {
+            _nextPositionX = 0;
+            _nextPositionY = 0;
+        }
     }
 }
+
